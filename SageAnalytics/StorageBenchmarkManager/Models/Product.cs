7a837fe12d167dbf6f3385b31953e075bca8e0e7
bobdelamater <bobdelamater@DEVENV>
2016-09-05 18:08:19 -0700

    Revert "Sample project improved slightly. Still not functional."
    This reverts commit 87a323fbc5f0f7e0bd3a47d32f2aa84a5de78038.

--- SageAnalytics/StorageBenchmarkManager/Models/Product.cs
+++ SageAnalytics/StorageBenchmarkManager/Models/Product.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StorageBenchmarkManager.Models
 {
@@ -13,10 +11,5 @@ namespace StorageBenchmarkManager.Models
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
-
-        [ForeignKey("Supplier")]
-        public string SupplierId { get; set; }
-        public virtual Supplier Supplier{ get; set; }
-
     }
 }
--- SageAnalytics/StorageBenchmarkManager/Models/StorageBenchmarkManagerContext.cs
+++ SageAnalytics/StorageBenchmarkManager/Models/StorageBenchmarkManagerContext.cs
@@ -14,13 +14,11 @@ namespace StorageBenchmarkManager.Models
         // automatically whenever you change your model schema, please use data migrations.
         // For more information refer to the documentation:
         // http://msdn.microsoft.com/en-us/data/jj591621.aspx
-
+    
         public StorageBenchmarkManagerContext() : base("name=StorageBenchmarkManagerContext")
         {
         }
 
         public System.Data.Entity.DbSet<StorageBenchmarkManager.Models.Product> Products { get; set; }
-
-        public System.Data.Entity.DbSet<StorageBenchmarkManager.Models.Supplier> Suppliers { get; set; }
     }
 }
